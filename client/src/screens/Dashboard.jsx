f1c6d4c32cfdae9225c9a1200bb6042dc4692e2a
Ansh Narula <2019210@iiitdmj.ac.in>
2022-02-04 16:06:54 +0530

    add user and user list frontend

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -1,33 +1,103 @@
-import React, {useEffect} from "react";
-import { Button, Card, Col, Container, Nav, Row } from "react-bootstrap";
-import {useSelector} from "react-redux";
+import React, { useEffect } from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Nav,
+  Row,
+  Tab,
+  Table,
+  Tabs,
+} from "react-bootstrap";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const {adminInfo} = useSelector(state => state.adminLogin)
+  const { adminInfo } = useSelector((state) => state.adminLogin);
   useEffect(() => {
     if (!adminInfo) {
-      navigate("/")
+      navigate("/");
     }
-  }, [adminInfo])
+  }, [adminInfo]);
   return (
     <Container>
-      <Row className="h-100">
+      <Row className=" my-5">
         <Col
           xs={10}
-          md={10}
-          className=" m-auto"
+          md={8}
+          className="mx-auto my-5 p-4"
           style={{ backgroundColor: "white" }}
         >
-          <Row className="p-2">
-            <Col md={6} className="text-center">
-              <Button>Click</Button>
-            </Col>
-            <Col md={6} className="text-center">
-              <Button> Click</Button>
-            </Col>
-          </Row>
+          <Tabs
+            defaultActiveKey="profile"
+            id="uncontrolled-tab-example"
+            className="mb-3"
+          >
+            <Tab eventKey="home" title="Add new user">
+              <Form>
+                <Row className="mb-3">
+                  <Form.Group as={Col}>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Username" />
+                  </Form.Group>
+
+                  <Form.Group as={Col}>
+                    <Form.Label>Mobile Number</Form.Label>
+                    <Form.Control type="text" placeholder="Mobile Number" />
+                  </Form.Group>
+                </Row>
+
+                <Form.Group className="mb-3" controlId="formGridAddress1">
+                  <Form.Label>Address</Form.Label>
+                  <Form.Control placeholder="1234 Main St" />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Email </Form.Label>
+                  <Form.Control placeholder="Enter email" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Tab>
+            <Tab eventKey="profile" title="All users">
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Username</th>
+                    <th>Mobile Number</th>
+                    <th>Address</th>
+                    <th>Email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <td >Larry the Bird</td>
+                    <td>@twitter</td>
+                    <td>@twitter</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Tab>
+          </Tabs>
         </Col>
       </Row>
     </Container>
